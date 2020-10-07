8e8b33925114fececf00038ca3296a4246bdbe5b
LuizHuallpa <luiz.huallpa@gmail.com>
2020-10-07 15:21:33 -0300

    Melhorando a galeria e adicionando Modal
    Signed-off-by: LuizHuallpa <luiz.huallpa@gmail.com>

--- js/java.js
+++ js/java.js
@@ -14,3 +14,18 @@ menssagem.addEventListener("submit", function(e) {
         alert('Campo não preenchido')
     }
 })
+
+
+
+$(function() {
+    var selectedClass = "";
+    $(".filter").click(function(){
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").fadeTo(100, 0.1);
+    $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+    setTimeout(function() {
+    $("."+selectedClass).fadeIn().addClass('animation');
+    $("#gallery").fadeTo(300, 1);
+    }, 300);
+    });
+    });
